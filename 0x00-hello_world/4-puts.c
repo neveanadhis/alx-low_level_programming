b8641c3c775e9359bb46df9f3c9b833584fffd32
Nevean Adhiambo <beatricemuga53@gmail.com>
2023-10-05 15:35:43 -0700

    Modified file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: no arg
+ *
+ * Description: printing without printf
+ *
+ * Return: a value of 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
