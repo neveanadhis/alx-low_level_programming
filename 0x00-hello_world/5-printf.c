6ed4a09653648897882a1622387dadce226fa885
Nevean Adhiambo <beatricemuga53@gmail.com>
2023-10-05 15:55:56 -0700

    Modified file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: no arg
+ *
+ * Description: prints with printf
+ *
+ * Return: value of 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
