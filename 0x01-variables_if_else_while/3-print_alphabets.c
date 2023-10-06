6cb9852275512bf2166de5acbdf7b73c79d8459f
Nevean Adhiambo <beatricemuga53@gmail.com>
2023-10-06 16:35:16 -0700

    Tasks 0-9

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: always0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
